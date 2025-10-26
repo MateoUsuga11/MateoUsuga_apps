5e8c2aa3539e364fec5827a0040f17398e8728d0
MateoUsuga11 <musugav1@eafit.edu.co>
2025-10-26 00:56:42 -0500

    Update URL for interaction link in Intro.py

--- Intro.py
+++ Intro.py
@@ -79,7 +79,7 @@ with col3:
  image = Image.open('OIG6.jpg')
  st.image(image, width=200)
  st.write("En la siguiente enlace veremos la capacidad de interacción con el mundo físico.") 
- url = "https://vision2-gpt4o.streamlit.app/"
+ url = "https://ctrlvoice-matthew.streamlit.app/"  
  st.write(f"Vision: [Enlace]({url})")
 
 
